f3726fdf7ee18158c71e3a787e17359aff8a84c1
Cicilia Marindi Putri <cicilia.putri@sigma.co.id>
2018-06-10 22:19:18 +0700

    Base app

---
+++ src/forum/Forum.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forum;
+
+/**
+ *
+ * @author user
+ */
+public class Forum {
+
+   public static void main(String args[]) {
+      String palindrome = "Hi! My Name is Cicilia";
+      int len = palindrome.length();
+      System.out.println( "String Length is : " + len );
+   }
+}
